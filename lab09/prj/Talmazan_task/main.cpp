30a015cd79090d4a54ae3c9a6ca461bce2c3607b
Sergey Talmazan <sdrakon927@gmail.com>
2021-04-13 08:28:26 +0300

    Fixed issue with Ukrainian letter 'i'

--- lab09/prj/Talmazan_task/main.cpp
+++ lab09/prj/Talmazan_task/main.cpp
@@ -42,16 +42,16 @@ void function8_1(){
 
 void function9_1(){
 
-    wcout << L"\n Уведіть силу вітру: ";
+    wcout << L"\n Уведiть силу вiтру: ";
     cin >> input;
 
     while (input < 0){
 
-        wcout << L"\n Помилка....Уведіть силу вітру ще раз: ";
+        wcout << L"\n Помилка....Уведiть силу вiтру ще раз: ";
         cin >> input;
     }
 
-    wcout << L"\n Сила вітру у м/с - " << wind_speed(input) << "\n";
+    wcout << L"\n Сила вiтру у м/с - " << wind_speed(input) << "\n";
     wcout << L"\n Бал Бофорта - " << beaufort_point(input) << "\n";
 }
 
@@ -62,12 +62,12 @@ void function9_2(){
 
         while(true){
 
-            wcout << L"\n Введiть середні температурні показники повітря (вiд -100 до 100) за " << i + 1 << L" день: ";
+            wcout << L"\n Введiть середнi температурнi показники повiтря (вiд -100 до 100) за " << i + 1 << L" день: ";
 
             if(!scanf("%f", &tem_arr[i])){
 
                 scanf("%*[^\n]");
-                wcout << L"\n Помилка....Будь ласка введіть число.\n";
+                wcout << L"\n Помилка....Будь ласка введiть число.\n";
             }
 
             if(tem_arr[i] >= -100 && tem_arr[i] <= 100){
@@ -76,7 +76,7 @@ void function9_2(){
 
             }else{
 
-                wcout << L"\n Помилка....Таких середніх температурних показників повiтря не iснує.";
+                wcout << L"\n Помилка....Таких середнiх температурних показникiв повiтря не iснує.";
                 wcout << L"\n Будь ласка спробуйте ще раз.\n";
             }
         }
@@ -84,7 +84,7 @@ void function9_2(){
 
     mid_tem_week(tem_arr, 7, &mid_temperature_celsius, &mid_temperature_farengates);
 
-    wcout << L"\n Середньотижнева температура за шкалою Цельсія - " << mid_temperature_celsius << "\n";
+    wcout << L"\n Середньотижнева температура за шкалою Цельсiя - " << mid_temperature_celsius << "\n";
     wcout << L"\n Середньотижнева температура за шкалою Фаренгейта - " << mid_temperature_farengates << "\n";
 }
 
@@ -93,12 +93,12 @@ void function9_3(){
 
     while(true){
 
-        wcout << L"\n Введiть цiле число в інтервалі вiд 0 до 70700: ";
+        wcout << L"\n Введiть цiле число в iнтервалi вiд 0 до 70700: ";
 
         if(!scanf("%d", &N)){
 
             scanf("%*[^\n]");
-            wcout << L"\n Помилка....Будь ласка введіть число.\n";
+            wcout << L"\n Помилка....Будь ласка введiть число.\n";
 
         }else if(N >= 0 && N <= 70700){
 
@@ -119,42 +119,42 @@ int main(){
     char input = 0;
 
     do {
-        wcout << L"\n Для виклику функції s_calculation натисніть \"q\", Для виклику функції до задачі 9.1 натисніть \"e\"\n";
-        wcout << L"\n Для виклику функції до задачі 9.2 натисніть \"r\", Для виклику функції до задачі 9.3 натисніть \"t\"\n\n";
+        wcout << L"\n Для виклику функцiї s_calculation натиснiть \"q\", Для виклику функцiї до задачi 9.1 натиснiть \"e\"\n";
+        wcout << L"\n Для виклику функцiї до задачi 9.2 натиснiть \"r\", Для виклику функцiї до задачi 9.3 натиснiть \"t\"\n\n";
         cin >> input;
 
         if (input == 'q' || input == 'Q') {
 
             function8_1();
 
-            wcout << L"\n\n Для того, щоб вийти натисніть \"y\", \"Y\" або \"H\": " << "\n";
+            wcout << L"\n\n Для того, щоб вийти натиснiть \"y\", \"Y\" або \"H\": " << "\n";
             cin >> input;
 
         }else if (input == 'e' || input == 'E'){
 
             function9_1();
 
-            wcout << L"\n\n Для того, щоб вийти натисніть \"y\", \"Y\" або \"H\": " << "\n";
+            wcout << L"\n\n Для того, щоб вийти натиснiть \"y\", \"Y\" або \"H\": " << "\n";
             cin >> input;
 
         }else if (input == 'r' || input == 'R'){
 
             function9_2();
 
-            wcout << L"\n\n Для того, щоб вийти натисніть \"y\", \"Y\" або \"H\": " << "\n";
+            wcout << L"\n\n Для того, щоб вийти натиснiть \"y\", \"Y\" або \"H\": " << "\n";
             cin >> input;
 
         }else if (input == 't' || input == 'T'){
 
             function9_3();
 
-            wcout << L"\n\n Для того, щоб вийти натисніть \"y\", \"Y\" або \"H\": " << "\n";
+            wcout << L"\n\n Для того, щоб вийти натиснiть \"y\", \"Y\" або \"H\": " << "\n";
             cin >> input;
 
         }else if (input != 'y' && input != 'Y' && input != 'h' && input != 'H'){
 
             Beep(417, 500);
-            wcout << L"\n Ви ввели невірний символ" << "\n";
+            wcout << L"\n Ви ввели невiрний символ" << "\n";
         }
 
     }while (input != 'y' && input != 'Y' && input != 'h' && input != 'H');
